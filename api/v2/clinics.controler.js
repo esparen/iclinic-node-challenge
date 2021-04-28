a7cfb4319f2caf83d726f98de3bdf6adc4e1a506
ederson <ederson@kiina.ai>
2021-04-28 17:49:23 -0300

    Added clinics.controler and its integration test; API is now sucessfully request data from the clinics service

---
+++ api/v2/clinics.controler.js
@@ -0,0 +1,84 @@
+const axios = require('axios');
+const config = require('config');
+const defaultMessages = require('../../config/messages.json');
+
+
+let externalAPI = config.get('external.api.clinics');
+
+/**
+ * @name getClinic 
+ * @param {number} clinicId the id of the clinic
+ * @returns an object with clinic's data or an error
+ */
+ async function getClinic(clinicId) {
+    let response = await requestClinicsAPI(clinicId);
+    return response;
+}
+
+/**
+ * @name requestClinicAPI
+ * @description uses Axios to make a request to the Clinic external API
+ * @param {number} clinicId the clinic ID
+ * @returns {JSON} an object with clinic's data or an error
+ */
+async function requestClinicsAPI(clinicId, customAxiosConfig = null) {
+    let axiosConfig;
+    //customAxiosConfig is used to perform automated tests in the API
+    if (customAxiosConfig) axiosConfig = customAxiosConfig
+    else
+        axiosConfig = {
+            method: 'GET',
+            baseURL: externalAPI.baseURL,
+            url: externalAPI.path.replace(':id', clinicId),
+            headers: externalAPI.default_headers,
+            timeout: externalAPI.timeout
+        }
+
+    let axiosResponse;
+    let response = { status: 0, found: false, data: {} }
+    try {
+        axiosResponse = await axios(axiosConfig);
+        response.status = 200;
+        response.found = true;
+        response.data = axiosResponse.data;
+    } catch (error) {
+        response = await formatClinicApiErrors(error);
+    } finally {
+        return response;
+    }
+}
+
+
+/**
+ * @name formatClinicApiErrors
+ * @description Handles the formating of errors from calling the Clinic API. Expected errors: 404 (not found); ECONNABORTED (axios timeout)
+ * @param {JSON} error the Axios error when the Clinic API was called
+ * @returns {JSON} the object containing the information of the error
+ */
+async function formatClinicApiErrors(error) {
+    //start assuming on unexpected and unhandled error.
+    let apiErrorResponse = {};
+    apiErrorResponse.status = defaultMessages.api.v2.prescription.errors.unexpected_error.error.code;
+    apiErrorResponse.found = false;
+    apiErrorResponse.data = defaultMessages.api.v2.prescription.errors.unexpected_error;
+    apiErrorResponse.data.original_error = error.message;
+
+    //timeout handling
+    if (error.code == 'ECONNABORTED') {
+        //clinics don't have error handling for service not available (yet). We pass empty data, but with the proper internal status code
+        apiErrorResponse.data = {}
+        apiErrorResponse.status = defaultMessages.api.v2.prescription.errors.clinics_service_not_available.error.code;
+    } else if (error.response) {
+        apiErrorResponse.data = {}
+        apiErrorResponse.status = error.response.status; 
+        //clinics don't have error handling for 404 - not found (yet). We pass empty data, but with the proper internal status code
+        if (error.response.status == 404)
+            apiErrorResponse.status = defaultMessages.api.v2.prescription.errors.clinic_not_found.error.code;
+    }
+    return apiErrorResponse;
+}
+
+module.exports = {
+    getClinic,
+    requestClinicsAPI
+}
---
+++ tests/integration/api/v2/clinics.controler.test.js
@@ -0,0 +1,50 @@
+const clinicsControler = require('../../../../api/v2/clinics.controler')
+const mockClinics = require('../../../mocks/clinics.mock.json');
+const defaultMessages = require('../../../../config/messages.json');
+const config = require('config');
+let externalAPI = config.get('external.api.clinics');
+
+describe('clinics.controler', () => {
+    describe('getClinic', () => {
+        mockClinics.forEach(mockedClinic => {
+            it(`should return an json with status 200. sample: ${JSON.stringify(mockedClinic)}`, async () => {
+                const res = await clinicsControler.getClinic(mockedClinic.id);
+                expect(res).toHaveProperty('status', 200);
+            });
+        })
+
+        it(`should return and json with status ${defaultMessages.api.v2.prescription.errors.clinic_not_found.error.code} and an empty message`, async () => {
+            const res = await clinicsControler.getClinic(5646465);
+            expect(res).toHaveProperty('status', defaultMessages.api.v2.prescription.errors.clinic_not_found.error.code);
+            expect(res).toHaveProperty('data', {});
+        });
+    });
+
+    describe('requestClinicsAPI', () => {
+        mockClinics.forEach(mockedClinic => {
+            it(`should return an json with status 200. sample: ${JSON.stringify(mockedClinic)}`, async () => {
+                const res = await clinicsControler.requestClinicsAPI(mockedClinic.id);
+                expect(res).toHaveProperty('status', 200);
+            });
+        });
+
+        it(`should return and json with status ${defaultMessages.api.v2.prescription.errors.clinic_not_found.error.code} and an empty message`, async () => {
+            const res = await clinicsControler.requestClinicsAPI(5646465);
+            expect(res).toHaveProperty('status', defaultMessages.api.v2.prescription.errors.clinic_not_found.error.code);
+            expect(res).toHaveProperty('data', {});
+        });
+
+        it(`should return and json with status ${defaultMessages.api.v2.prescription.errors.clinics_service_not_available.error.code} and and empty message`, async () => {
+            let customAxiosConfig = {
+                method: 'GET',
+                baseURL: externalAPI.baseURL,
+                url: externalAPI.path.replace(':id', 1),
+                headers: externalAPI.default_headers,
+                timeout: 1
+            }
+            const res = await clinicsControler.requestClinicsAPI(5646465, customAxiosConfig);
+            expect(res).toHaveProperty('status', defaultMessages.api.v2.prescription.errors.clinics_service_not_available.error.code);
+            expect(res).toHaveProperty('data', {});
+        });
+    });
+});
