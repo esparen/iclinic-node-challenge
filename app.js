78f395e35deda15a0a4ece581c0bc6882f110fe7
ederson <ederson@kiina.ai>
2021-04-28 11:18:01 -0300

    Added express options for bodyparser and urlencoder

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 app.use(morgan('combined'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 require('./api/routes')(app);
 require('./db')(app);
